db30239b845ffd0e1e713daa631240e7f4ff4ef4
KYUNGHOON MIN <kyunghoon451@gmail.com>
2020-12-17 14:59:18 +0900

    [feat] implement github/gitlab webhook validation (#45)
    * [feat] implement github/gitlab setting commit status logic
    
    * make manifests generate added
    
    * [refactor] change set commit status function parameter
    
    * [refactor] move commit status body structure
    
    * [refactor] change code reflecting reviews
    
    * [refactor] change code to close response body
    
    * [feat] implement github/gitlab webhook validation
    
    * [feat] Implement IntegrationConfig controller (#33)
    
    * [feat] Implement IntegrationConfig controller
    
    - Add IntegrationConfig controller which generates secrets and registers
      webhook
    - Add Ingress controller which watches ingress and set external hostname
      referring to the ingress hostname
    
    * [feat] Pipeline status reflector (#35)
    
    * [feat] Implement pipeline status reflector
    
    * [chore] Add extra fields for CRDs
    
    * [fix] Fix function call for setting commit status (#39)
    
    * [feat] Add report server implementation (#41)
    
    * [feat] Add report server implementation
    
    - Report is used for commit status detail page
    
    * [feat] Add Approval CRD (#42)
    
    This CRD is for approving tasks. This is a user-facing CRD where
    requested approvers check and decide to approve or reject following
    tasks.
    
    * [feat] implement github/gitlab webhook validation
    
    * [refactor] handle errors for lint
    
    * [refactor] change hashPayload return values
    
    Co-authored-by: kyunghoon_min <kyunghoon_min@tmax.co.kr>
    Co-authored-by: Sunghyun Kim <cqbqdd11519@gmail.com>

--- pkg/git/github/github.go
+++ pkg/git/github/github.go
@@ -2,6 +2,8 @@ package github
 
 import (
 	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -23,9 +25,14 @@ type CommitStatusBody struct {
 	Context     string `json:"context"`
 }
 
-func (c *Client) ParseWebhook(header http.Header, jsonString []byte) (git.Webhook, error) {
-	var eventType = git.EventType(header.Get("X-Github-Event"))
+func (c *Client) ParseWebhook(integrationConfig *cicdv1.IntegrationConfig, header http.Header, jsonString []byte) (git.Webhook, error) {
 	var webhook git.Webhook
+	var signature = strings.Replace(header.Get("x-hub-signature"), "sha1=", "", 1)
+	if err := Validate(integrationConfig.Status.Secrets, signature, jsonString); err != nil {
+		return webhook, err
+	}
+	var eventType = git.EventType(header.Get("x-github-event"))
+
 	var err error
 	if eventType == git.EventTypePullRequest {
 		var data PullRequestWebhook
@@ -160,5 +167,30 @@ func (c *Client) SetCommitStatus(integrationJob *cicdv1.IntegrationJob, integrat
 		return fmt.Errorf("error setting commit status, code %d, msg %s", resp.StatusCode, string(body))
 	}
 
+	return nil
+}
+
+func IsValidPayload(secret, headerHash string, payload []byte) bool {
+	hash := HashPayload(secret, payload)
+	return hmac.Equal(
+		[]byte(hash),
+		[]byte(headerHash),
+	)
+}
+
+func HashPayload(secret string, payloadBody []byte) string {
+	hm := hmac.New(sha1.New, []byte(secret))
+	_, err := hm.Write(payloadBody)
+	sum := hm.Sum(nil)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%x", sum)
+}
+
+func Validate(secret, headerHash string, payload []byte) error {
+	if !IsValidPayload(secret, headerHash, payload) {
+		return fmt.Errorf("invalid request : X-Hub-Signature does not match secret")
+	}
 	return nil
 }
