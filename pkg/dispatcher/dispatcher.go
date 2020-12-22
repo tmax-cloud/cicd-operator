9e0e820d3768a15c19c700c1459312c473d39f9f
kyunghoon_min <kyunghoon_min@tmax.co.kr>
2020-12-22 16:28:25 +0900

    [wip] implement cicd dispatcher

--- pkg/dispatcher/dispatcher.go
+++ pkg/dispatcher/dispatcher.go
@@ -1,9 +1,13 @@
 package dispatcher
 
 import (
+	"context"
 	"fmt"
+
 	cicdv1 "github.com/tmax-cloud/cicd-operator/api/v1"
+	"github.com/tmax-cloud/cicd-operator/internal/utils"
 	"github.com/tmax-cloud/cicd-operator/pkg/git"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -15,19 +19,118 @@ type Dispatcher struct {
 }
 
 func (d Dispatcher) Handle(webhook git.Webhook, config *cicdv1.IntegrationConfig) error {
+	var job *cicdv1.IntegrationJob
+	pr := webhook.PullRequest
+	push := webhook.Push
+	if pr == nil && push == nil {
+		return fmt.Errorf("pull request and push struct is nil")
+	}
+
+	jobId := utils.RandomString(20)
+	jobName := fmt.Sprintf("%s-%s-%s", config.Name, pr.Head.Sha[:5], jobId[:5])
+
+	jobs, err := filterJobs(webhook, config)
+	if err != nil {
+		return err
+	}
+	if pr != nil && push == nil {
+		job = handlePullRequest(webhook, config, jobId, jobName, jobs)
+	} else if push != nil && pr != nil {
+		job = handlePush(webhook, config, jobId, jobName, jobs)
+	}
+	if err := d.Client.Create(context.Background(), job); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handlePullRequest(webhook git.Webhook, config *cicdv1.IntegrationConfig, jobId, jobName string, jobs []cicdv1.Job) *cicdv1.IntegrationJob {
+	pr := webhook.PullRequest
+	job := &cicdv1.IntegrationJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      jobName,
+			Namespace: config.Namespace,
+			Labels:    map[string]string{}, // TODO
+		},
+		Spec: cicdv1.IntegrationJobSpec{
+			ConfigRef: cicdv1.IntegrationJobConfigRef{
+				Name: config.Name,
+				Type: cicdv1.JobTypePreSubmit,
+			},
+			Id:   jobId,
+			Jobs: jobs,
+			Refs: cicdv1.IntegrationJobRefs{
+				Repository: webhook.Repo.Name,
+				Link:       webhook.Repo.URL,
+				Base: cicdv1.IntegrationJobRefsBase{
+					Ref:  pr.Base.Ref,
+					Link: webhook.Repo.URL,
+				},
+				Pull: &cicdv1.IntegrationJobRefsPull{
+					Id:   pr.ID,
+					Ref:  pr.Head.Ref,
+					Sha:  pr.Head.Sha,
+					Link: pr.URL,
+					Author: cicdv1.IntegrationJobRefsPullAuthor{
+						Name: pr.Sender.Name,
+					},
+				},
+			},
+		},
+	}
+
+	return job
+}
+
+func handlePush(webhook git.Webhook, config *cicdv1.IntegrationConfig, jobId, jobName string, jobs []cicdv1.Job) *cicdv1.IntegrationJob {
+	push := webhook.Push
+	job := &cicdv1.IntegrationJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      jobName,
+			Namespace: config.Namespace,
+			Labels:    map[string]string{}, // TODO
+		},
+		Spec: cicdv1.IntegrationJobSpec{
+			ConfigRef: cicdv1.IntegrationJobConfigRef{
+				Name: config.Name,
+				Type: cicdv1.JobTypePostSubmit,
+			},
+			Id:   jobId,
+			Jobs: jobs,
+			Refs: cicdv1.IntegrationJobRefs{
+				Repository: webhook.Repo.Name,
+				Link:       webhook.Repo.URL,
+				Base: cicdv1.IntegrationJobRefsBase{
+					Ref:  push.Ref,
+					Link: webhook.Repo.URL,
+					Sha:  push.Sha,
+				},
+			},
+		},
+	}
+	return job
+}
+
+func filterJobs(webhook git.Webhook, config *cicdv1.IntegrationConfig) ([]cicdv1.Job, error) {
+	var jobs []cicdv1.Job
+
+	var cand []cicdv1.Job
 	switch webhook.EventType {
 	case git.EventTypePullRequest:
-		return d.handlePullRequest(webhook, config)
+		cand = config.Spec.Jobs.PreSubmit
 	case git.EventTypePush:
-		return d.handlePush(webhook, config)
+		cand = config.Spec.Jobs.PostSubmit
+	default:
+		return nil, fmt.Errorf("dispatcher cannot handle event %s", webhook.EventType)
 	}
-	return fmt.Errorf("dispatcher cannot handle event %s", webhook.EventType)
+
+	// TODO - filter
+	jobs = append(jobs, cand...)
+
+	return jobs, nil
 }
 
-func (d Dispatcher) handlePullRequest(webhook git.Webhook, config *cicdv1.IntegrationConfig) error {
-	return nil // TODO
-}
-
-func (d Dispatcher) handlePush(webhook git.Webhook, config *cicdv1.IntegrationConfig) error {
-	return nil // TODO
+func filter(cand []cicdv1.Job, webhook git.Webhook, config *cicdv1.IntegrationConfig) []cicdv1.Job {
+	return nil
 }
