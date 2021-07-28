45f5e0c5f00f25419c64e3421eb5a4e63b39f8fa
Sunghyun Kim <cqbqdd11519@gmail.com>
2021-07-28 16:04:00 +0900

    [chore] Add copyrights for all the files (#260)
    - Add copyrights
    - Add guide for the copyright insertion
    - Add code of conduct
    - Add contributing document
    - Regrouped import lines

---
+++ CONTRIBUTING.md
@@ -0,0 +1,6 @@
+# Contributing to CI/CD Operator
+
+- [Code of conduct](./CODE_OF_CONDUCT.md)
+
+- [Development flow](./docs/development/dev-flow.md)
+- [Release flow](./docs/development/release.md)
--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -1,17 +1,17 @@
 /*
+ Copyright 2021 The CI/CD Operator Authors
 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 */
 
 package main
@@ -19,6 +19,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
+
 	tektonv1alpha1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 	tektonv1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"github.com/tmax-cloud/cicd-operator/controllers/customs"
@@ -34,14 +37,12 @@ import (
 	"github.com/tmax-cloud/cicd-operator/pkg/git"
 	"github.com/tmax-cloud/cicd-operator/pkg/plugins/size"
 	"github.com/tmax-cloud/cicd-operator/pkg/server"
-	"io"
 	rbac "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	apiregv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
-	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
