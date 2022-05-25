fc0d8d0ed8e2ec6ffbd0bf826cd45bd8c45361d4
S-hayeon <hayeona92@gmail.com>
2022-05-25 14:16:10 +0900

    [Fix] support git insecure option

--- pkg/pipelinemanager/step_git_checkout.go
+++ pkg/pipelinemanager/step_git_checkout.go
@@ -1,12 +1,9 @@
 /*
  Copyright 2021 The CI/CD Operator Authors
-
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
      http://www.apache.org/licenses/LICENSE-2.0
-
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,14 +34,11 @@ func gitCheckout() tektonv1beta1.Step {
 	step.Script = `#!/bin/sh
 set -x
 set -e
-
 git config --global user.email "bot@cicd.tmax.io"
 git config --global user.name "tmax-cicd-bot"
 git init
-
 CHECKOUT_URL="$CI_SERVER_URL/$CI_REPOSITORY"
 CI_HEAD_REF_ARRAY="$CI_HEAD_REF"
-
 if [ "$CI_BASE_REF" = "" ]; then 
   # Push Event
   CHECKOUT_REF="$CI_HEAD_REF"
@@ -52,19 +46,16 @@ else
   # Pull Request Event
   CHECKOUT_REF="$CI_BASE_REF"
 fi
-
-git fetch "$CHECKOUT_URL" "$CHECKOUT_REF"
-git checkout FETCH_HEAD
-
+git -c http.sslVerify=false fetch "$CHECKOUT_URL" "$CHECKOUT_REF"
+git -c http.sslVerify=false checkout FETCH_HEAD
 if [ "$CI_BASE_REF" != "" ]; then
   # Pull request event
   for ci_head_ref in $CI_HEAD_REF_ARRAY; do 
-    git fetch "$CHECKOUT_URL" "$ci_head_ref"
-    git merge --no-ff FETCH_HEAD
+    git -c http.sslVerify=false fetch "$CHECKOUT_URL" "$ci_head_ref"
+    git -c http.sslVerify=false merge --no-ff FETCH_HEAD
   done
 fi
-
-git submodule update --init --recursive
+git -c http.sslVerify=false submodule update --init --recursive
 `
 	resources := corev1.ResourceList{
 		"cpu":    resource.MustParse(gitCheckoutCPUReq),
